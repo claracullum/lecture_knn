a4371e7f6c1b960e3b4b50d6a5780519e5f4806d
Clara Cullum <claracullum@gmail.com>
2023-11-14 12:38:17 -0500

    end of lecture

--- run_knc.py
+++ run_knc.py
@@ -1,15 +1,18 @@
 import pandas
 
-from sklearn.neighbors import KNeighborsRegressor
+from sklearn.neighbors import KNeighborsClassifier
 import kfold_template
 
 dataset = pandas.read_csv("abalone.csv")
 dataset = dataset.sample(frac=1)
 
+dataset['Age Group'] = pandas.cut(dataset['Rings'], [0,9,11,13,100], label=[1,2,3,4])
+dataset = dataset.drop(['Rings'], axis=1)
+
 # gives us 8th column
 target = dataset.iloc[:,8]
-# ages is rings +1.5
-target = target+1.5
+# ages is rings +1.5, so we dont need it in this program 
+# target = target+1.5
 target = target.values 
 
 data = dataset.iloc[:,0:8]
@@ -17,13 +20,13 @@ pandas.get_dummies(data, columns = ["Sex"])
 data = data.values 
 # print(data)
 
-#true is bc its continuous 
+#false is bc not continuous 
 
 trials = []
-for w in ["uniform","distance"]
+for w in ["uniform","distance"]:
 	for k in range(1,50):
-		machine = KNeighborsRegressor(n_neighbors=10, weights="uniform")
-		return_values = kfold_template.run_kfold(machine, data, target, 4, True)
+		machine = KNeighborsClassifier(n_neighbors=10, weights="uniform")
+		return_values = kfold_template.run_kfold(machine, data, target, 4, False)
 		average_return_value = sum(return_values)/len(return_values)
 		# print(average_return_values)
 		trials.append((average_return_value, k, w))
